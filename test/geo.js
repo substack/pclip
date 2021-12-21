0396677e850677183ced94e23df048e38b4e6fdc
substack <substack@bits.coop>
2021-12-20 20:21:52 -1000

    another geo test

--- test/geo.js
+++ test/geo.js
@@ -1,11 +1,7 @@
 var test = require('tape')
 var peq = require('polygon-eq')
 var pclip = require('../')
-var geo = {
-  intersect: require('intersect-great-circle'),
-  pointInPolygon: require('geo-point-in-polygon'),
-  distance: require('haversine-distance'),
-}
+var geo = require('../geo')
 
 test('two triangles geodetic clip', function (t) {
   var A = [[0,0],[5,8],[10,0]]
@@ -22,6 +18,30 @@ test('two triangles geodetic clip', function (t) {
   ], 1e-3), 'union')
   t.ok(peq(pclip.difference(A,B,geo), [
     [ [ [ 6.246, 6.026 ], [ 5, 8 ], [ 0, 0 ], [ 10, 0 ], [ 7.512, 4.004 ], [ 5, 4 ] ] ],
+  ], 1e-3), 'difference')
+  t.end()
+})
+
+test('clipping over the north pole', function (t) {
+  var A = [[-120,+61],[+0,+60],[+120,59]]
+  var B = [[-156,+74],[+171,+75],[+21,+85]]
+  t.ok(peq(pclip.intersect(A,B,geo), [
+    [[-156,+74],[+171,+75],[+21,+85]]
+  ], 1e-3), 'intersect')
+  t.ok(peq(pclip.exclude(A,B,geo), [
+    [
+      [[-120,+61],[+0,+60],[+120,59]],
+      [[-156,+74],[+171,+75],[+21,+85]],
+    ],
+  ], 1e-3), 'exclude')
+  t.ok(peq(pclip.union(A,B,geo), [
+    [[-120,+61],[+0,+60],[+120,59]]
+  ], 1e-3), 'union')
+  t.ok(peq(pclip.difference(A,B,geo), [
+    [
+      [[-120,+61],[+0,+60],[+120,59]],
+      [[-156,+74],[+171,+75],[+21,+85]],
+    ],
   ], 1e-3), 'difference')
   t.end()
 })
