f57b0ddba5798498bd86ba97e7bf0ff97ed7b561
substack <substack@bits.coop>
2021-12-27 11:27:12 -1000

    fix case by using point in polygon on lerp result

--- xy.js
+++ xy.js
@@ -2,5 +2,5 @@ module.exports = {
   intersect: require('line-segment-intersect-2d'),
   pointInPolygon: require('point-in-polygon'),
   distance: require('gl-vec2/distance'),
-  dot: require('gl-vec2/dot'),
+  lerp: require('gl-vec2/lerp'),
 }
