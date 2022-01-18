ccdb964e9a46fd7c6fb8a8dd41b6df83d09c0656
substack <substack@bits.coop>
2022-01-17 22:50:13 -1000

    all tests pass without hacky lerp thing

--- test/xy.js
+++ test/xy.js
@@ -922,7 +922,8 @@ test('box inside box with shared corner', function (t) {
     [[[0,5],[5,5],[5,0],[2,0],[2,2],[0,2]]],
   ], 1e-3), 'exclude')
   t.ok(
-    peq(pclip.union(A,B,xy), [[[[0,0],[0,5],[5,5],[5,0]]]], 1e-3),
+    peq(pclip.union(A,B,xy), [[[[0,0],[0,5],[5,5],[5,0]]]], 1e-3)
+    || peq(pclip.union(A,B,xy), [[[[0,0],[0,2],[0,5],[5,5],[5,0],[2,0]]]], 1e-3),
     'union'
   )
   t.ok(peq(pclip.difference(A,B,xy), [
@@ -944,9 +945,11 @@ test('box inside box with shared corner and hole', function (t) {
     [[[0,5],[5,5],[5,0],[2,0],[2,2],[0,2]]],
     [[[1,1.5],[1,1],[1.5,1],[1.5,1.5]]],
   ], 1e-3), 'exclude')
-  t.ok(peq(pclip.union(A,B,xy), [
-    [[[0,0],[0,5],[5,5],[5,0]]]
-  ], 1e-3), 'union')
+  t.ok(
+    peq(pclip.union(A,B,xy), [[[[0,0],[0,5],[5,5],[5,0]]]], 1e-3)
+    || peq(pclip.union(A,B,xy), [[[[0,0],[0,2],[0,5],[5,5],[5,0],[2,0]]]], 1e-3),
+    'union'
+  )
   t.ok(peq(pclip.difference(A,B,xy), [
     [[[1,1.5],[1,1],[1.5,1],[1.5,1.5]]],
   ], 1e-3), 'difference')
